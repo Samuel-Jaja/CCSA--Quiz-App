6a380af7962611d115ba77eba61b5f57d5b6b02d
Samuel Jaja <samuel.jaja21@gmail.com>
2022-08-07 20:12:54 +0100

    Vuex Update

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,31 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import Axios from "axios";
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        questions: [],
+    },
+    getters: {
+        questions: function(state) {
+            return state.questions;
+        },
+        currentQuestion: function(state) {
+            return state.currentQuestion;
+        },
+    },
+    mutations: {
+        setQuestions(state, payload) {
+            state.questions = payload;
+        },
+        currentQuestion(state, payload) {
+            state.currentQuestion = payload;
+        },
+    },
+    actions: {
+        async loadQuestions({ commit }) {
+            const response = await Axios.get("https://opentdb.com/api.php?amount=10");
+            commit("setQuestions", response.data.results);
+        },
+    },
+    // modules: {},
+});
